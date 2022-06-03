4875768286086c6f7791aea0a85662005e69a25d
Camila Lo <camilalo.tw@gmail.com>
2022-06-03 13:51:57 +0800

    use presvious

--- practice-react/src/components/Todo.js
+++ practice-react/src/components/Todo.js
@@ -1,8 +1,21 @@
-import React, {useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 export default function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState('');
+
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -27,6 +40,7 @@ export default function Todo(props) {
         type="text" 
         value={newName}
         onChange={handleChange}
+        ref={editFieldRef}
       />
     </div>
     <div className="btn-group">
@@ -59,7 +73,12 @@ const viewTemplate = (
         </label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn" onClick={() => setEditing(true)}>
+        <button 
+          type="button" 
+          className="btn" 
+          onClick={() => setEditing(true)}
+          ref={editFieldRef}
+        >
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
         <button
@@ -73,8 +92,16 @@ const viewTemplate = (
   </div>
 );
 
+useEffect(() => {
+  if (!wasEditing && isEditing) {
+    editFieldRef.current.focus();
+  }
+  if (wasEditing && !isEditing) {
+    editButtonRef.current.focus();
+  }
+}, [wasEditing, isEditing]);
+
     return (
-        <li className="todo">{isEditing ? editingTemplate : viewTemplate}
-        </li>
+        <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>
     );
 }
